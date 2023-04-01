39e46a39362e64cf3407d661060976c68255b2c5
VTarunaKumar <vtarunakumarrao@gmail.com>
2023-04-01 19:48:08 +0530

    Initialize the app

---
+++ android/app/src/main/kotlin/com/shiva/flutter_voice_assistant_chatgpt_dall_e/flutter_voice_assistant_chatgpt_dall_e/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shiva.flutter_voice_assistant_chatgpt_dall_e.flutter_voice_assistant_chatgpt_dall_e
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
