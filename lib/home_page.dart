bbea7e80e452c264786e4cbc3d576496dc23410d
V Taruna Kumar Rao <vtarunakumarrao@gmail.com>
2023-04-20 14:46:15 +0530

    Adding Asset

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,1 +1,14 @@
-import 'material'
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
